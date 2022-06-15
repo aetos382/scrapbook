0b0bdc1c8c6f31472100eb31312e838b32397ffb
aetos <aetos382@users.noreply.github.com>
2022-06-15 14:19:06 +0900

    INITIAL COMMIT

---
+++ chrome/webpack.config.js
@@ -0,0 +1,26 @@
+const { join } = require('path');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    contentPage: join(__dirname, 'src/contentPage.ts'),
+    serviceWorker: join(__dirname, 'src/serviceWorker.ts')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  output: {
+    path: join(__dirname, '../angular/dist'),
+    filename: '[name].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  }
+};
